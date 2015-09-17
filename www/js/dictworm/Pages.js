2f2698d9949394c30d4220b2c09b440dcdf7ef11
Kodlee Yin <kodlee@kodleeshare.net>
2015-09-16 23:35:24 -0700

    Cleaned up initial file structure; Added libraries; Setting up base layout for app

---
+++ www/js/dictworm/Pages.js
@@ -0,0 +1,10 @@
+/**
+ * Defines the pages within DictionaryWorm
+ */
+var Pages = function() {
+	this.something = 1;
+
+	this.method = function() {
+		console.log(this.something++);
+	}
+};
--- www/js/index.js
+++ www/js/index.js
@@ -3,6 +3,7 @@ var app = {
     initialize: function() {
         this.bindEvents();
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -10,6 +11,7 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
@@ -17,6 +19,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
